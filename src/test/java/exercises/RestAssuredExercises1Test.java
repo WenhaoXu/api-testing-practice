03ae962aa983650cefc77e67140aa731f561f829
XUHO3 <howells.xu@oocl.com>
2018-07-30 10:40:30 +0800

    checkResponseCodeForCorrectRequest complete

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -60,8 +61,8 @@ public class RestAssuredExercises1Test {
 
         given().
                 spec(requestSpec).
-                when().
-                then();
+                when().get("/2016/drivers.json").
+                then().contentType(ContentType.JSON);
     }
 
     /***********************************************
